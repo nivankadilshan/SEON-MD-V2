2dc727977667bd1fc341420352ba1ba78d241137
nivankadilshan <109707385+nivankadilshan@users.noreply.github.com>
2024-12-23 08:54:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'SEON-MD=HJ0nXBxD#3Z1UfMD3PeScYX0zcI7winDt_N4L5-8_F2GNEIukAWU' : process.env.SESSION_ID,
 GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'nivankadilshan': process.env.GITHUB_USERNAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'Put your github auth token': process.env.GITHUB_AUTH_TOKEN
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN
 };
